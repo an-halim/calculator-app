036fbcf1ed9dbe8666134a68d5ae6c630b884b47
an-halim <jcklark12@gmail.com>
2021-03-09 12:12:25 +0700

    Penambahan beberapa function

--- calculator.cs
+++ calculator.cs
@@ -13,6 +13,15 @@ namespace Calculator
         static int tambah (int a, int b){
             return a+b;
         }
+        static int kurang (int a, int b){
+            return a-b;
+        }
+        static int kali (int a, int b){
+            return a*b;
+        }
+        static int bagi (int a, int b){
+            return a/b;
+        }
         
     }
 }
